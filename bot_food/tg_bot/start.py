95b675335a6ed508e6241ed08c3c6b3b9f49b48b
Aleksan-dres <galeks382@gmail.com>
2025-10-29 18:06:55 +0300

    Update start.py
    Добавил кнопку о количестве оплаченных подписок

--- bot_food/tg_bot/start.py
+++ bot_food/tg_bot/start.py
@@ -6,32 +6,31 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-ATTEMPT_LIMIT = 3
+ATTEMPT_LIMIT = 3  # Максимальное число попыток
+SUBSCRIBED = False  # Флаг подписки
 
 
-def start(update: Update, context: CallbackContext):
-    """Обработчик команды /start — приветственное сообщение и главное меню."""
+def start(update: Update, context: CallbackContext): 
+  """Обработчик команды /start — приветственное сообщение и главное меню."""
     context.user_data['attempts_left'] = ATTEMPT_LIMIT
     context.user_data['subscribed'] = False
-    welcome_text = (
-        "Добро пожаловать в FoodPlanner!\n\n" 
-        "Это кулинарный бот.\n"
-        "Здесь вы найдете вкусные рецепты и сможете готовить как шеф-повар .\n"
-        "Начнем?\n\n"
-    )
-
+    update.message.reply_text("Добро пожаловать в FoodPlanner!\n\n"
+                              "Это кулинарный бот.\n"
+                              "Здесь вы найдете вкусные рецепты и сможете готовить как шеф-повар .\n"
+                              "Начнем?\n\n")
     keyboard = [
         [
             InlineKeyboardButton("Случайное блюдо", callback_data='dish'),
-            InlineKeyboardButton("Оформить подписку", callback_data='subscribe'),
+            InlineKeyboardButton("Офорить подписку",
+                                 callback_data='subscribe'),
         ],
-        [InlineKeyboardButton("О нас", callback_data='info')],
+        [InlineKeyboardButton("О нас", callback_data='info'),
+         InlineKeyboardButton("Оплаченных подписок",
+                              callback_data='statistics')
+         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Пожалуйста, выберите:',
+                              reply_markup=reply_markup)
 
-    update.message.reply_text(
-      text=welcome_text,
-      parse_mode='Markdown',
-      reply_markup=reply_markup
-    )
 
