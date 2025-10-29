3c354cc27153591ae9039b21dd8c30bb25ad87a3
amee886 <nsecex@gmail.com>
2025-10-29 15:18:06 +0300

    почистил код

--- bot_food/tg_bot/company_information.py
+++ bot_food/tg_bot/company_information.py
@@ -1,19 +1,24 @@
 from telegram import Update, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import  CallbackContext 
 
+
 def info(query: Update, context: CallbackContext):
-    
+    """Отправляет информацию о команде и кнопку возврата в меню."""
     query.answer()
     
-    query.edit_message_text(
-        text="Команда Романа и Ларисы — это творческий дуэт увлечённых кулинаров, чья страсть к приготовлению вкусных блюд нашла отражение в обширной библиотеке рецептов в Instagram.\n"
-             "За годы своего существования они собрали внушительную коллекцию кулинарных идей,\n"
-             "каждая из которых отличается оригинальностью и простотой исполнения.",
-        parse_mode='Markdown'
+    text = (
+        "Команда Романа и Ларисы — это творческий дуэт увлечённых кулинаров, чья страсть к приготовлению вкусных блюд нашла отражение в обширной библиотеке рецептов в Instagram.\n"
+        "За годы своего существования они собрали внушительную коллекцию кулинарных идей,\n"
+        "каждая из которых отличается оригинальностью и простотой исполнения.",
     )
-    keyboard = [ 
-       
-    [InlineKeyboardButton("В меню", callback_data='start')],
-    ] 
+    
+    keyboard = [
+        [InlineKeyboardButton("В меню", callback_data='start')]
+    ]
     reply_markup = InlineKeyboardMarkup(keyboard) 
-    query.edit_message_reply_markup(reply_markup=reply_markup)
+    
+    query.edit_message_text(
+        text=text,
+        parse_mode='Markdown',
+        reply_markup=reply_markup
+    )
