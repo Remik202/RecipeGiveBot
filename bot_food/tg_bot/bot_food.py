c268f5572690cc2d4368f3cc28a3f1cb1e5af5f9
Aleksan-dres <galeks382@gmail.com>
2025-10-29 17:56:34 +0300

    Update bot_food.py
    Добавил еще статистику оплаты

--- bot_food/tg_bot/bot_food.py
+++ bot_food/tg_bot/bot_food.py
@@ -1,20 +1,19 @@
-import os
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from dotenv import load_dotenv
-from tg_bot.start import *
-from tg_bot.recipes import dish, show_recipe
-from tg_bot.subscription_stats import subscribe, check_payment
-from tg_bot.company_information import info
+import os 
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler 
+from dotenv import load_dotenv   
+from tg_bot.start import * 
+from tg_bot.recipes import dish, show_recipe 
+from tg_bot.subscription_stats import subscribe, check_payment 
+from tg_bot.company_information import info 
+from tg_bot.statistics import statistics_bot 
 import logging
-
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-load_dotenv()
-BOT_TOKEN = os.getenv("RECIPE_GIVE_BOT")
-
+load_dotenv() 
+token_bot = os.getenv("RECIPE_GIVE_BOT") 
+user_data= {}
 
 def handle_button_click(update, context):
     """Обработчик нажатия на кнопку"""
@@ -24,36 +23,41 @@ def handle_button_click(update, context):
     if data == 'dish':
         dish(query, context)
     elif data == 'info':
-        info(query, context)
-    elif data == "start":
-        start(query, context)
+        info(query, context) 
+    elif data ==  "start": 
+        start(query, context) 
     elif query.data == 'next_dish':
         dish(query, context)
     elif query.data == 'show_recipe':
-        show_recipe(query, context)
-    elif query.data == 'subscribe':
+        show_recipe(query, context) 
+    elif query.data == 'subscribe': 
         subscribe(query, context)
-    elif data == 'check_payment':
-        check_payment(query, context)
+    elif data == 'check_payment':  
+        check_payment(query, context) 
+    elif data == "statistics": 
+        statistics_bot(query,context)
 
-
-def main():
-    """Запуск Telegram-бота"""
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
     
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("dish", dish))
-    dispatcher.add_handler(CommandHandler("info", info))
-    dispatcher.add_handler(CommandHandler("show_recipe", show_recipe))
+
+def main(): 
+    updater = Updater(token = token_bot, use_context=True) 
+    dispatcher = updater.dispatcher 
+    dispatcher.add_handler(CommandHandler("start", start)) 
+    dispatcher.add_handler(CommandHandler("dish", dish)) 
+    dispatcher.add_handler(CommandHandler("info", info)) 
+    dispatcher.add_handler(CommandHandler("show_recipe", show_recipe)) 
+
     dispatcher.add_handler(CallbackQueryHandler(handle_button_click))
     
-    updater.start_polling()
-    updater.idle()
 
+
+
+    updater.start_polling()
+    updater.idle() 
 
 if __name__ == "__main__":
     main()
+
 
 
 
