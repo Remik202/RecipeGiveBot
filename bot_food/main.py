70d27e3fa0cec85a45c210ac3c159f259df646fa
amee886 <nsecex@gmail.com>
2025-10-29 16:58:05 +0300

    почистил код

--- bot_food/main.py
+++ bot_food/main.py
@@ -6,7 +6,6 @@ from tg_bot.bot_food import main
 current_dir = Path(__file__).parent
 sys.path.append(str(current_dir))
 
-from tg_bot.bot_food import main
 
 if __name__ == "__main__":
-    main()
+main()
